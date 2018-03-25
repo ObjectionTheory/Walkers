ec22aa1003e7bca684bf187e2da97a17181c0847
ObjectionTheory <therandomguy265@gmail.com>
2018-03-25 22:16:04 +0100

    T

--- HostCode/setup.py
+++ HostCode/setup.py
@@ -1,10 +1,10 @@
 import servo_controller as servo
 import time
 
-basePositions = [50, 20, 20, 50, 30, 180, 180, 30, 50, 130, 50, 130]
+basePositions = [50, 20, 20, 50, 30, 180, 180, 30, 50, 130, 50,     130]
 
 servos = [servo.Servo(i) for i in range(12)]
 
-for i, j in servos, range(12):
-    i.set_angle(basePositions[j])
+for i in range(12):
+    servos[i].set_angle(basePositions[i])
     time.sleep(1)
