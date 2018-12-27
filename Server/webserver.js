8eafb8f99fa8e1b4e23fd5c5a21a61a3d6d8a8f1
ObjectionTheory <therandomguy265@gmail.com>
2018-12-27 17:55:25 +0000

    PWM

--- Server/webserver.js
+++ Server/webserver.js
@@ -1,10 +1,26 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
-var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+var i2cBus = require("i2c-bus");
+var drier = require("pca9685").Pca9685Driver;
+var options = {
+    i2c: i2cBus.openSync(1),
+    address: 0x40,
+    frequency: 60,
+    debug: false
+};
 
+pwm = new Pca9685Driver(options, function(err) {
+    if (err) {
+        console.error("Error initializing PCA9685");
+        process.exit(-1);
+    }
+    for (var i = 0; i < 12; i ++) {
+        pwm.setPulseLength(i, 1500);
+    }
+    console.log("Initialization done");
+ 
+});
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
@@ -21,25 +37,19 @@ function handler (req, res) { //create server
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   var lightvalue = 0; //static variable for current status
-  pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton
-    if (err) { //if an error
-      console.error('There was an error', err); //output error message to console
-      return;
-    }
-    lightvalue = value;
-    socket.emit('light', lightvalue); //send button status to client
-  });
+  
   socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
-    if (lightvalue != LED.readSync()) { //only change LED if status has changed
-      LED.writeSync(lightvalue); //turn LED on or off
-    }
+    pwm.setPulseLength(0, 1300);
+    
   });
+  socket.on("reset", function() {
+      for (var i = 0; i < 12; i ++) {
+          pwm.setPulseLength(i, 1500);
+      }
+  })
 });
 
 process.on('SIGINT', function () { //on ctrl+c
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport LED GPIO to free resources
-  pushButton.unexport(); // Unexport Button GPIO to free resources
+  
   process.exit(); //exit completely
 });
